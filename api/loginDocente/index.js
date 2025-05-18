6114c3dfa9107a3d8b319a9d332d142007204eef
rescolies <58514612+rescolies@users.noreply.github.com>
2025-05-18 16:37:44 +0200

    debug2

--- api/loginDocente/index.js
+++ api/loginDocente/index.js
@@ -2,6 +2,17 @@ const bcrypt = require('bcryptjs');
 const jwt    = require('jsonwebtoken');
 
 module.exports = async function(context, req) {
+  if (req.query.debug === '1') {
+    context.res = {
+      status: 200,
+      body: {
+        JWT_SECRET: process.env.JWT_SECRET,
+        DOCENTE_PW_HASH: process.env.DOCENTE_PW_HASH,
+        passwordReceived: req.body?.password
+      }
+    };
+    return;
+  }
   const pw = req.body?.password;
   if (!pw) {
     context.res = { status: 400, body: 'Falta el campo password' };
