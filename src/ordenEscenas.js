8f401a839fd0ae0ec192e64d5e320ce1dd1d2546
rescolies <58514612+rescolies@users.noreply.github.com>
2025-05-30 02:38:44 +0200

    ordenEscenas

--- src/ordenEscenas.js
+++ src/ordenEscenas.js
@@ -1,42 +1,144 @@
 // src/ordenEscenas.js
 const ordenEscenas = [
   {
-    nivel: 1, // [cite: 796]
+    nivel: 1,
     categorias: {
-      social: [],
-      // Removed 'oler' and 'pluja' from this list
-      emocionpropia: ['perroSentarse','apagarLuz','psicomotricidad','pegatina','chocarCinco','globoExplota','busTarde','canastaAplaudir','chupachup','relojCaer', 'llamarProfe'],
-      teoriamente: ['avionRysa','ivan','mirar'],
-      coherencia: ['excursion','naturaleza','piscina','cumpleaños','estacionAño','dondeVan','bancAliments','cocinar','cine'], // [cite: 797]
-      ejecutiva: ['cambioRutina','paris','cambioMantas','pan'],
-      memoria: ['pintarMesa2','guardarTablet'],
-      caras: [],
+      social: [
+        'patatas',
+        'juguete',
+        'jugueteRotoMisterio',
+        'noDejanJugarFutbol',
+        'quiereJugueteAjeno',
+        'manchaDibujoSinQuerer',
+        'cogeTijerasMaestra',
+        'cogeMagdalenaAntesTiempo',
+        'cogeZumoAjenoEnfado',
+        'invitacionJugarFutbol',
+        'esperarTurnoBano',
+        'noCompartenGalleta',
+        'unirseJuegoParque'
+      ],
+      emocionpropia: [
+        'perroSentarse',
+        'apagarLuz',
+        'psicomotricidad',
+        'pegatina',
+        'chupachup',
+        'chocarCinco',
+        'globoExplota',
+        'relojCaer',
+        'busTarde',
+        'canastaAplaudir',
+        'llamarProfe'
+      ],
+      teoriamente: [
+        'avionRysa',
+        'ivan',
+        'mirar'
+      ],
+      coherencia: [ //
+        'excursion',
+        'naturaleza',
+        'piscina',
+        'cumpleaños',
+        'estacionAño',
+        'dondeVan',
+        'bancAliments',
+        'cocinar',
+        'cine'
+      ],
+      ejecutiva: [
+        'cambioRutina',
+        'paris',
+        'cambioMantas',
+        'pan'
+      ],
+      memoria: [
+        'pintarMesa2',
+        'guardarTablet'
+      ],
+      caras: []
     }
   },
   {
     nivel: 2,
     categorias: {
-      social: [], // [cite: 798]
-      emocionpropia: ['pelotaRota','seOlvidaDeTi','cremalleraRota','responderMal','hacerFoto','mancharse', 'mochilaRota','ventanaAvion','profeAmor','botonPlay'],
-      teoriamente: ['erupto','abrazo','chocarFuerte'],
-      coherencia: ['cumpleaños2','estacionAño2','aeropuerto','queHacen','estacionAño3'],
-      ejecutiva: ['papaMedico','mochila','herida','mojado'],
-    } // [cite: 799]
+      social: [ //
+        'aniversario',
+        'pastelPequenoFiesta',
+        'sinFelicitacionClase',
+        'regaloNoGustado',
+        'escondeZapatosBroma',
+        'ignoraOrdenDocente',
+        'quiereOtroJuegoGrupo',
+        'errorEnJuegoCorregido'
+      ],
+      emocionpropia: [
+        'pelotaRota',
+        'seOlvidaDeTi',
+        'responderMal',
+        'hacerFoto',
+        'mancharse',
+        'cremalleraRota',
+        'mochilaRota',
+        'ventanaAvion',
+        'profeAmor',
+        'botonPlay'
+      ],
+      teoriamente: [
+        'erupto',
+        'abrazo',
+        'chocarFuerte'
+      ],
+      coherencia: [
+        'cumpleaños2',
+        'aeropuerto',
+        'queHacen',
+        'estacionAño2',
+        'estacionAño3'
+      ],
+      ejecutiva: [
+        'papaMedico',
+        'mochila',
+        'herida',
+        'mojado'
+      ]
+    }
   },
   {
     nivel: 3,
     categorias: {
       social: [],
-      emocionpropia: ['vacaciones','elegirFruta','ventanaFuerte','repartidor','equivocar','charco','nuevoProfe'], // [cite: 800]
-      teoriamente: ['perroRafa','pintarMesa','claseB'],
-      coherencia: ['museo','comedor'],
-      ejecutiva: ['cumpleañosFamiliar','dentista','pedirAyuda','verano'],
+      emocionpropia: [ //
+        'repartidor',
+        'equivocar',
+        'charco',
+        'nuevoProfe',
+        'vacaciones',
+        'elegirFruta',
+        'ventanaFuerte'
+      ],
+      teoriamente: [
+        'perroRafa',
+        'pintarMesa',
+        'claseB'
+      ],
+      coherencia: [
+        'museo',
+        'comedor'
+      ],
+      ejecutiva: [
+        'cumpleañosFamiliar',
+        'dentista',
+        'pedirAyuda',
+        'verano'
+      ]
     }
   }
 ];
 
 export const obtenerSecuenciaEscenas = () => {
-  const secuencia = []; // [cite: 801]
+  const secuencia = []; //
   ordenEscenas.forEach(nivelObj => {
     Object.values(nivelObj.categorias).forEach(categoriaEscenas => {
       secuencia.push(...categoriaEscenas);
@@ -46,40 +148,40 @@ export const obtenerSecuenciaEscenas = () => {
 };
 
 export const esUltimaEscenaDelNivel = (escenaIdActual, escenasDelNivel) => {
-    if (!escenasDelNivel || escenasDelNivel.length === 0) return false; // [cite: 802]
+    if (!escenasDelNivel || escenasDelNivel.length === 0) return false; //
     return escenasDelNivel[escenasDelNivel.length - 1] === escenaIdActual;
 };
 
 export const obtenerEscenasDelNivelActual = (escenaIdActual, todasLasEscenas) => {
     const escenaActualObj = todasLasEscenas.find(e => e.id === escenaIdActual);
-    if (!escenaActualObj) return []; // [cite: 803]
+    if (!escenaActualObj) return []; //
 
     const nivelActualConfig = ordenEscenas.find(n => n.nivel === escenaActualObj.nivel);
     if (!nivelActualConfig) return [];
 
     let escenasEnNivel = [];
     // Iterate in the defined order of categories to maintain consistency
-    const categoriasOrdenadas = Object.keys(nivelActualConfig.categorias); 
+    const categoriasOrdenadas = Object.keys(nivelActualConfig.categorias);
     categoriasOrdenadas.forEach(catKey => {
         escenasEnNivel.push(...(nivelActualConfig.categorias[catKey] || []));
     });
     return escenasEnNivel;
 };
 
-export const esPrimeraEscenaDelNivel = (escenaIdActual, ordenEscenasConfig, todasLasEscenas) => { // [cite: 804]
+export const esPrimeraEscenaDelNivel = (escenaIdActual, ordenEscenasConfig, todasLasEscenas) => { //
     const escenaActualObj = todasLasEscenas.find(e => e.id === escenaIdActual);
     if (!escenaActualObj) return false;
 
     const nivelActualConfig = ordenEscenasConfig.find(n => n.nivel === escenaActualObj.nivel);
     if (!nivelActualConfig) return false;
 
-    for (const catKey in nivelActualConfig.categorias) { // [cite: 805]
+    for (const catKey in nivelActualConfig.categorias) { //
         const escenasEnCategoria = nivelActualConfig.categorias[catKey];
-        if (escenasEnCategoria && escenasEnCategoria.length > 0) { // [cite: 806]
+        if (escenasEnCategoria && escenasEnCategoria.length > 0) { //
             return escenasEnCategoria[0] === escenaIdActual;
         }
     }
-    return false; // [cite: 807]
+    return false; //
 };
 
 
